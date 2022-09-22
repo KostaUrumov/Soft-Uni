1f4d6bed9ca8d068116b18df3df9fdc82d691f16
KostaUrumov <koceurumov@gmail.com>
2022-09-22 15:38:03 +0000

    new one

---
+++ C# Findamentials/Methods/Math operations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            int numberTwo = int.Parse(Console.ReadLine());
+            Console.WriteLine(Operation(numberOne,  numberTwo, operation));
+
+        }
+
+        static int Operation(int numberOne, int numberTwo, string operation)
+        {
+            int result = 0;
+            if (operation == "+")
+            {
+                result = numberOne + numberTwo;
+            }
+
+            else if (operation == "-")
+            {
+                result = numberOne - numberTwo;
+
+            }
+
+            else if (operation == "*")
+            {
+                result = numberOne * numberTwo;
+            }
+
+            else if (operation == "/")
+            {
+                result = numberOne / numberTwo;
+            }
+
+            return result;
+        }
+    }
+}
