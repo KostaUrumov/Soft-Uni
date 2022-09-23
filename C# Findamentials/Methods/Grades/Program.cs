eef72aa1cf23a0a99bdab2aafe9753fb0ef364f5
KostaUrumov <koceurumov@gmail.com>
2022-09-23 12:00:29 +0000

    new Exercises

---
+++ C# Findamentials/Methods/Grades/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace I._Declaring_and_Invoking_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeChecker();
+        }
+        static void GradeChecker()
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if (grade < 3.00)
+            {
+                Console.WriteLine("Fail");
+            }
+
+            else if (grade > 3.00 && grade <= 3.49)
+            {
+                Console.WriteLine("Poor");
+            }
+
+            else if (grade > 3.49 && grade <= 4.49)
+            {
+                Console.WriteLine("Good");
+            }
+
+            else if (grade > 4.49 && grade <= 5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else if (grade > 5.49 && grade <= 6.00)
+            {
+                Console.WriteLine("Excellent");
+            }
+
+        }
+
+
+    }
+}
---
+++ C# Findamentials/Methods/Multiply Evens by Odds/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _10._Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Math.Abs (int.Parse(Console.ReadLine()));
+            int numberOdd = GetOddSum(number, 1);
+            int numberEven = GetEvenSum(number, 1);
+            Console.WriteLine(Multiply(numberOdd, numberEven));
+        }
+
+
+        static int GetOddSum(int number, int odd)
+        {
+            int digit = 0;
+            int oddSum = 0;
+
+            while (number > 0)
+            {
+                digit = number % 10;
+                number = number / 10;
+                if (digit % 2 == 1)
+                {
+                    oddSum += digit;
+                }
+
+            }
+            return oddSum;
+
+        }
+
+
+        static int GetEvenSum(int number, int even)
+        {
+            int digit = 0;
+            int evenSum = 0;
+
+            while (number > 0)
+            {
+                digit = number % 10;
+                number = number / 10;
+                if (digit % 2 == 0)
+                {
+                    evenSum += digit;
+                }
+
+            }
+            return evenSum;
+
+        }
+
+
+        static int Multiply(int numberOdd, int numberEven)
+        {
+            int result = numberOdd * numberEven;
+            return result;
+        }
+
+
+
+
+
+
+    }
+}
