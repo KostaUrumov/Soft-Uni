8b15271b51d5c0a9d85f37b0899e616312185be3
KostaUrumov <koceurumov@gmail.com>
2022-09-28 08:03:48 +0000

    more exercises

---
+++ C# Findamentials/Methods/Add and Subtract/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _5._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            int numberThree = int.Parse(Console.ReadLine());
+            int result = AddNumbers(numberOne, numberTwo, numberThree);
+
+            Console.WriteLine(MinusNumbers(numberOne, numberTwo, numberThree));
+            
+        }
+
+        private static int MinusNumbers(int numberOne, int numberTwo, int numberThree)
+        {
+
+            int result = AddNumbers(numberOne, numberTwo, numberThree) - numberThree;
+            return result;
+        }
+
+        static int AddNumbers(int numberOne, int numberTwo, int numberThree)
+        {
+            
+
+            int sum =  numberOne + numberTwo;
+            return sum;
+        }
+
+    }
+}
---
+++ C# Findamentials/Methods/Characters in Range/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _3._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CarectersInRange('a', 'b');
+        }
+
+        static void CarectersInRange(char a , char b)
+        {
+
+          a = char.Parse(Console.ReadLine());
+          b = char.Parse(Console.ReadLine());
+
+            int numberOne = a;
+            int numberTwo = b;
+
+            if (numberTwo < numberOne)
+            {
+                numberOne = numberTwo;
+                numberTwo = a;
+            }
+
+            
+
+            for (int i = numberOne + 1; i < numberTwo; i++)
+            {
+                char c = (char)i;
+                Console.Write($"{c} ");
+            }
+
+            
+
+        }
+    }
+}
