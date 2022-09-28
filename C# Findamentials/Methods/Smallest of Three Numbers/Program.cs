c9cbf815023f902cb808f876a89f88b75fb371ff
KostaUrumov <koceurumov@gmail.com>
2022-09-28 08:01:10 +0000

    added

---
+++ C# Findamentials/Methods/Smallest of Three Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MethodExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BiggestNumberChecker(); 
+        }
+
+        private static void BiggestNumberChecker()
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            int numberThree = int.Parse(Console.ReadLine());
+
+
+            if (numberOne <= numberTwo && numberOne <= numberThree)
+            {
+                Console.WriteLine(numberOne);
+            }
+
+            else if (numberTwo <= numberOne && numberTwo <= numberThree)
+            {
+                Console.WriteLine(numberTwo);
+            }
+
+            else if (numberThree <= numberOne && numberThree <= numberTwo)
+            {
+                Console.WriteLine(numberThree);
+            }
+        }
+
+    }
+}
---
+++ C# Findamentials/Methods/Vowels Count/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _2._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            Console.WriteLine(NumberOfVowels(word));
+        }
+
+        static int NumberOfVowels(string word)
+        {
+            int vowel = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u' || word[i] == 'A' || word[i] == 'E' || word[i] == 'I' || word[i] == 'O' || word[i] == 'U')
+                {
+                    vowel++;
+                }
+            }
+            
+            return vowel;
+
+        }
+    }
+}
