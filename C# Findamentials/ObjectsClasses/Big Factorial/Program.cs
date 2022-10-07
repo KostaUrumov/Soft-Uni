e3e91b3d47f9e98f432f511fa7a574749d781be3
KostaUrumov <koceurumov@gmail.com>
2022-10-07 11:08:32 +0000

    Big Factorial/2. Big Factorial.csproj

---
+++ C# Findamentials/ObjectsClasses/Big Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace _2._Big_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer = int.Parse(Console.ReadLine());
+            BigInteger bigInteger = integer;
+            for (int i = 2; i < integer; i++)
+            {
+                bigInteger *= i;
+            }
+
+            Console.WriteLine(bigInteger);
+
+        }
+    }
+}
---
+++ C# Findamentials/ObjectsClasses/Songs/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._Songs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int songsNumber = int.Parse(Console.ReadLine());
+
+            List<Song> list = new List<Song>();
+            for (
+                int i = 0; i < songsNumber; i++)
+            {
+                string songLine = Console.ReadLine();
+                string [] songData = songLine.Split('_');
+
+
+                Song song = new Song();
+                song.TypeList = songData[0];
+                song.Name = songData[1];
+                song.time = (songData[2]);
+
+                
+                list.Add(song);
+            }
+
+            
+           CallSongs(list);
+
+           static void CallSongs(List<Song> list)
+            {
+                
+             
+                string playSong = Console.ReadLine();
+
+                //if (playSong == list[i].TypeList)
+                //{
+
+                //}
+
+                if (playSong == "all")
+                {
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        Console.WriteLine(list[i].Name);
+
+                    }
+                }
+
+                else if (playSong != "all")
+                {
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i].TypeList == playSong)
+                        {
+                            Console.WriteLine(list[i].Name);
+                        }
+
+                    }
+                }
+
+            }
+        }
+
+       
+    }
+}
