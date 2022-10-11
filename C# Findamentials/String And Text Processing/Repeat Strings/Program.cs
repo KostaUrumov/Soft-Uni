6855858547d652b432e8d3db93c5dac8c31f4cf5
KostaUrumov <koceurumov@gmail.com>
2022-10-11 08:29:37 +0000

    lkj

---
+++ C# Findamentials/String And Text Processing/Repeat Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            string [] commandSplit = command.Split(' ');
+            string newString = string.Empty;
+            foreach (var moni in commandSplit)
+            {
+                if (moni.Contains(moni))
+                {
+                    for (int i = 0; i < moni.Length; i++)
+                    {
+                        newString += moni;
+                    }
+                }
+            }
+            Console.WriteLine(newString);
+            
+        }
+    }
+}
---
+++ C# Findamentials/String And Text Processing/StringAndTextLab/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringAndTextLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if(command == "end")
+                {
+                    break;
+                }
+                List<char> reversedCommand = new List<char>();
+                for (int i = command.Length-1; i >= 0; i--)
+                {
+                    reversedCommand.Add(command[i]);
+                }
+
+                string [] reverse = new string [reversedCommand.Count];
+                for (int i = 0; i < reversedCommand.Count; i++)
+                {
+                    reverse[i] = reversedCommand[i].ToString();
+                }
+
+                Console.Write($"{command} = ");
+                Console.Write(String.Join("", reverse));
+                Console.WriteLine();
+            }
+        }
+    }
+}
