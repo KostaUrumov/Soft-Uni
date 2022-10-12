db07ffe4d2110ad4eef7cd3f05405f8312ea32b0
KostaUrumov <koceurumov@gmail.com>
2022-10-12 18:00:26 +0000

    hgfbf

---
+++ C# Findamentials/String And Text Processing/Character Multiplier/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2._Character_Multiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = Console.ReadLine();
+            Console.WriteLine(CalculateNumber(one));
+        }
+
+        static int CalculateNumber(string one)
+        {
+            string[] properties = one.Split(' ');
+            int result = 0;
+            int toAdd = 0;
+            
+            if (properties[0].Length > properties[1].Length)
+            {
+                for (int i = 0; i < properties[0].Length; i++)
+                {
+                    if (i < properties[1].Length)
+                    {
+                       toAdd = properties[0][i] * properties[1][i];
+                       result+= toAdd;
+                        continue;
+                    }
+                    if (i >= properties[1].Length)
+                    {
+                        result+=properties[0][i];
+                    }
+                    
+                }
+            }
+
+            if (properties[0].Length <= properties[1].Length)
+            {
+                for (int i = 0; i < properties[1].Length; i++)
+                {
+                    if (i < properties[0].Length)
+                    {
+                        toAdd = properties[0][i] * properties[1][i];
+                        result += toAdd;
+                        continue;
+                    }
+                    if (i >= properties[0].Length)
+                    {
+                        result += properties[1][i];
+                    }
+                    
+                }
+
+            }
+
+            return result; 
+        }
+    }
+}
---
+++ C# Findamentials/String And Text Processing/Extract File/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3.__Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string path = Console.ReadLine();
+            List<string> file = new List<string>();
+           string extention = string.Empty;
+            int stop = 0; 
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (path[i] == 46)
+                {
+                    stop = i;
+                    
+                }
+            }
+
+            for (int i = stop + 1; i < path.Length; i++)
+            {
+                extention+=path[i];
+            }
+
+            for (int i = stop -1 ; i >= 0; i--)
+            {
+                if (path[i] != 92)
+                {
+                    file.Add(path[i].ToString());
+                }
+                if (path[i] == 92)
+                {
+                    break;
+                }
+            }
+            file.Reverse();
+            Console.WriteLine($"File name: {string.Join("",file)}");
+            Console.WriteLine($"File extension: {extention}");
+
+        }
+    }
+}
