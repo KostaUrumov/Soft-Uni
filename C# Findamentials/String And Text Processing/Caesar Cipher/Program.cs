c36a11e03bc55963da0ccd585da766fd475dcbec
KostaUrumov <koceurumov@gmail.com>
2022-10-13 08:42:30 +0000

    jjj

---
+++ C# Findamentials/String And Text Processing/Caesar Cipher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _4.__Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            StringBuilder encryptedMessage = new StringBuilder();
+            for (int i = 0; i < message.Length; i++)
+            {
+                char c = message[i];
+                char s = Convert.ToChar(c + 3 );
+                encryptedMessage.Append(s);
+            }
+
+            Console.WriteLine(encryptedMessage);
+        }
+    }
+}
---
+++ C# Findamentials/String And Text Processing/Replace Repeating Chars/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace _6.__Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            
+            StringBuilder result = new StringBuilder();
+            result.Append(command[0]);
+
+            for (int i = 0; i < command.Length; i++)
+            {
+                
+                if (command[i] != command[i + 1])
+                { 
+                    result.Append(command[i +1 ]);
+                }
+                if (i + 1 == command.Length - 1)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
