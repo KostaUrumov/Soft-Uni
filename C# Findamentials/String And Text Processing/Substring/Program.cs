9c447b7bf82469c483c80c9dc9145f6cf21b2f28
KostaUrumov <koceurumov@gmail.com>
2022-10-15 15:30:33 +0000

    123

---
+++ C# Findamentials/String And Text Processing/Substring/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _3._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           string signsToBeRemoved = Console.ReadLine();
+           string commnadToManipulate = Console.ReadLine();
+            while (true)
+            {
+                int firstSingofSignsToBeRemoved = commnadToManipulate.IndexOf(signsToBeRemoved);
+                if (firstSingofSignsToBeRemoved == -1)
+                {
+                    break;
+                }
+                else 
+                {
+                    commnadToManipulate = commnadToManipulate.Remove(firstSingofSignsToBeRemoved, signsToBeRemoved.Length);
+                }
+            }
+            Console.WriteLine(commnadToManipulate);
+        }
+    }
+}
---
+++ C# Findamentials/String And Text Processing/Text Filter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _4._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bannedWordsEntry = Console.ReadLine();
+            string [] banned = bannedWordsEntry.Split(", ");
+            string textToClear = Console.ReadLine();
+            
+
+            for (int i = 0; i < banned.Length; i++)
+            {
+                string toReplance = string.Empty;
+                while (true)
+                {
+                    int number = textToClear.IndexOf(banned[i]);
+                    for (int h = 0; h < banned[i].Length; h++)
+                    {
+                        toReplance += "*";
+                    }
+                    if (number == -1)
+                    {
+                        break;
+
+                    }
+                    else
+                    {
+                        textToClear = textToClear.Replace(banned[i].ToString(), toReplance);
+                    }
+                    
+                }
+            }
+
+            Console.WriteLine(textToClear);
+        }
+    }
+}
