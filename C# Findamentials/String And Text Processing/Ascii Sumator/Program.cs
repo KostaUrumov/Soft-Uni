9ad24f08accf365886f865c54dd8f829b6ba60dd
KostaUrumov <koceurumov@gmail.com>
2022-10-18 08:04:33 +0000

    kl;

---
+++ C# Findamentials/String And Text Processing/Ascii Sumator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace _2._Ascii_Sumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char one = char.Parse(Console.ReadLine());
+            char two = char.Parse(Console.ReadLine());
+
+            int numberone = Convert.ToInt32(one);
+            int numbertwo = Convert.ToInt32(two);
+            string commandLine = Console.ReadLine();
+            int result = 0;
+
+            if (numberone < numbertwo)
+            for (int i = 0; i < commandLine.Length; i++)
+            {
+                    if (commandLine[i] > numberone && commandLine[i] < numbertwo)
+                    {
+                        result+= Convert.ToInt32(commandLine[i]);
+                    }
+            }
+
+            if (numberone > numbertwo)
+            {
+
+
+                for (int i = 0; i < commandLine.Length; i++)
+                {
+                    if (commandLine[i] > numbertwo && commandLine[i] < numberone)
+                    {
+                        result += Convert.ToInt32(commandLine[i]);
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ C# Findamentials/String And Text Processing/Treasure Finder/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3._Treasure_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] keys = Console.ReadLine().Split(' ');
+            string toAdd = string.Empty;
+            int asciiNumber = 0;
+            while (true)
+            {
+                
+                string phrase = Console.ReadLine();
+                int g = 0;
+                if (phrase == "find")
+                {
+                    break;
+                }
+                string result = string.Empty;
+
+                for (int i = 0; i < phrase.Length; i++)
+                {
+                    if (i < keys.Length)
+                    {
+                        asciiNumber = phrase[i];
+                        asciiNumber = asciiNumber - int.Parse(keys[i]);
+                        result += ((char)asciiNumber);
+                    }
+
+                    if (i >= keys.Length)
+                    {
+                        if (g != keys.Length)
+                        {
+                            asciiNumber = phrase[i];
+                            asciiNumber = asciiNumber - int.Parse(keys[g]);
+                            result += ((char)asciiNumber);
+                            g++;
+                            continue;
+                        }
+
+                        if (g == keys.Length)
+                        {
+                            g = 0;
+                            asciiNumber = phrase[i];
+                            asciiNumber = asciiNumber - int.Parse(keys[g]);
+                            result += ((char)asciiNumber);
+                            g++;
+                        }
+                      
+                    }
+                }
+
+                
+                int typeStart = result.IndexOf("&");
+                string removed = result.Remove(typeStart, 1);
+                typeStart = typeStart + 1;
+                int typeEnd = removed.IndexOf("&");
+                
+                string type = result.Substring(typeStart, typeEnd + 1 - typeStart);
+
+                int startCoordinates = result.IndexOf("<");
+                int endCoordinates = result.IndexOf(">");
+                string coordinates = result.Substring(startCoordinates + 1, endCoordinates - startCoordinates - 1);
+
+                Console.WriteLine($"Found {type} at {coordinates}");
+
+
+            }
+        }
+    }
+}
