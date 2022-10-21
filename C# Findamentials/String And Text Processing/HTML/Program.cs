dfaf565244276ec9e2a666077ef1fa0f37b4af9d
KostaUrumov <koceurumov@gmail.com>
2022-10-21 15:16:29 +0000

    ll

---
+++ C# Findamentials/String And Text Processing/HTML/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _5._HTML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string article = Console.ReadLine();
+            string content = Console.ReadLine();
+
+            Console.WriteLine("<h1>");
+            Console.WriteLine($"    {article}");
+            Console.WriteLine("</h1>");
+            Console.WriteLine("<article>");
+            Console.WriteLine($"    {content}");
+            Console.WriteLine("</article>");
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "end of comments")
+                {
+                    break;
+                }
+                if (command != "end of comments")
+                {
+                    Console.WriteLine("<div>");
+                    Console.WriteLine($"    {command}");
+                    Console.WriteLine("</div>");
+                }
+
+            }
+        }
+    }
+}
