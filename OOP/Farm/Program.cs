f5923cf5e72d9bea6e48e8973e3fda0c1a611a64
KostaUrumov <koceurumov@gmail.com>
2022-11-06 12:20:40 +0000

    ku,

---
+++ OOP/Farm/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+            Animal animal = new Animal();
+            animal.Eat();
+
+            Puppy puppy = new Puppy();
+
+            puppy.Weep();  
+
+
+        }
+    }
+}
+
