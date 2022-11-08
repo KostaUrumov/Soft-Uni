63cfc9beba57373a00b5c27286d237a8f1d52cb2
KostaUrumov <koceurumov@gmail.com>
2022-11-08 20:13:19 +0000

    dgfdfgb

---
+++ C# Advanced/Reverse a String/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reverse_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var command = Console.ReadLine();
+            Stack<string> reversed = new Stack<string>();
+            for (int i = 0; i < command.Length; i++)
+            {
+                reversed.Push(command[i].ToString());
+            }
+
+            while (reversed.Count >0)
+            {
+                Console.Write(reversed.Pop());
+
+            }
+
+        }
+    }
+}
---
+++ C# Advanced/StackSum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            Stack<int> integers = new Stack<int>(line.Split(" ").Select(int.Parse).ToArray());
+
+            string command = Console.ReadLine();
+            command = command.ToLower();
+
+            while (command != "end")
+            {
+                string[] commandParts = command.Split(" ");
+
+                if (commandParts[0] == "remove")
+                {
+                    if (int.Parse(commandParts[1]) < integers.Count)
+                    {
+                        for (int i = 0; i < int.Parse(commandParts[1]); i++)
+                        {
+                            integers.Pop();
+                        }
+                    }
+                }
+
+                if (commandParts[0] == "add")
+                {
+                    integers.Push(int.Parse(commandParts[1]));
+                    integers.Push(int.Parse(commandParts[2]));
+                }
+                command = Console.ReadLine();
+                command = command.ToLower();
+            }
+
+            Console.WriteLine($"Sum: {integers.Sum()}");
+        }
+    }
+}
