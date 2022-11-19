926dcb3c9b45bbe641f78c50f2df083839e544f0
KostaUrumov <koceurumov@gmail.com>
2022-11-19 19:58:50 +0000

    sdfv

---
+++ C# Advanced/Matching Brackets/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            List<string> answers = new List<string>();
+            string command = Console.ReadLine();
+            for (int i = 0; i < command.Length; i++)
+            {
+                if (command[i] == 40)
+                {
+                    stack.Push(i);
+                }
+                if (command[i] == 41)
+                {
+                    int number = stack.Pop();
+                    string subs = command.Substring(number, i - number+1);
+                    answers.Add(subs);
+
+                }
+            }
+            foreach (string s in answers)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ C# Advanced/Supermarket/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> shoppingLine  = new Queue<string>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command != "Paid" && command != "End")
+                {
+                    shoppingLine.Enqueue(command);
+                }
+                if (command == "Paid")
+                {
+                    while (shoppingLine.Count > 0)
+                    {
+                        Console.WriteLine(shoppingLine.Dequeue());
+                    }
+                }
+
+                if (command == "End")
+                {
+                    Console.WriteLine($"{shoppingLine.Count} people remaining.");
+                    break;
+                }
+            }
+        }
+    }
+}
