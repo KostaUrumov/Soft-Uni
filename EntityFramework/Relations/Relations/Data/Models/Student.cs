de5195113bbc32db0e24517c161f56ae8e5c2bf7
KostaUrumov <koceurumov@gmail.com>
2023-01-12 21:34:06 +0000

    addd

---
+++ EntityFramework/Relations/Relations/Data/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [MaxLength(80)]
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(10)]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public DateTime RegisteredOn { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+    }
+}
---
+++ EntityFramework/Relations/Relations/Data/StudentSystemContext .cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+
+        public StudentSystemContext()
+        {
+        }
+
+        public StudentSystemContext(DbContextOptions<StudentSystemContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-EMH9F7E;Database=StudentDB;Integrated Security=true");
+            }
+        }
+
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+    }
+}
