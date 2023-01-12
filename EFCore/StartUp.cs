62e97a99beb61c78e57e070ab1c08d0b5b1384db
Kostadin Urumov <koceurumov@gmail.com>
2023-01-12 13:15:41 +0200

    das

---
+++ EFCore/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoftUnidkjasbca
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
