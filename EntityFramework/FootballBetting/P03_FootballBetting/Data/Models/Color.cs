dc38b4187054d44937acd4e5afe91277372ce371
KostaUrumov <koceurumov@gmail.com>
2023-01-13 21:35:51 +0000

    more firld

--- EntityFramework/FootballBetting/P03_FootballBetting/Data/Models/Color.cs
+++ EntityFramework/FootballBetting/P03_FootballBetting/Data/Models/Color.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace P03_FootballBetting.Data.Models
 {
     public class Color
     {
+        [Key]
         public int ColorId { get; set; }
 
         public string Name { get; set; }
--- EntityFramework/FootballBetting/P03_FootballBetting/Data/Models/Team.cs
+++ EntityFramework/FootballBetting/P03_FootballBetting/Data/Models/Team.cs
@@ -1,24 +1,41 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
 using System.Text;
 
 namespace P03_FootballBetting.Data.Models
 {
     public class Team
     {
+        [Key]
         public int TeamID { get; set; }
-
-        public  string Name { get; set; }
+        [MaxLength(30)]
+        [Required]
+        public string Name { get; set; }
 
         public string LogoUrl { get; set; }
 
-        public string Initials { get; set; }
+        [MaxLength(3)]
+        [MinLength(3)]
+        public string? Initials { get; set; }
 
+        [Required]
         public decimal Budget { get; set; }
 
+        [Required]
+        [ForeignKey(nameof(ColorId))]
         public int PrimaryKitColorId { get; set; }
-        public int SecondaryKitColorId { get; set; }
+        public Color ColorPrimary { get; set; }
 
+        [ForeignKey(nameof(ColorId))]
+        public int SecondaryKitColorId { get; set; }
+        public Color ColorSecond { get; set; }
+
+        [ForeignKey(nameof(TownId))]
         public int TownId { get;}
+        public Town Town { get; set; }
     }
 }
