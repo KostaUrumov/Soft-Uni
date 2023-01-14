3c71957a8c9b2055236a4fd15fe775ae70d1a51c
Kostadin Urumov <koceurumov@gmail.com>
2023-01-14 11:56:56 +0200

    sad

--- EntityFramework/FootballBetting/P03_FootballBetting/Data/Models/Bet.cs
+++ EntityFramework/FootballBetting/P03_FootballBetting/Data/Models/Bet.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace P03_FootballBetting.Data.Models
 {
     public class Bet
     {
+        
+        [Key]
         public int BetId { get; set; }
+        [Required]
         public decimal Amount { get; set; }
+        [Required]
         public string Prediction { get; set; }
+        [Required]
         public DateTime DateTime { get; set; }
+        [ForeignKey(nameof(UserId))]
         public int UserId { get; set; }
+        public User User { get; set; }
+        [ForeignKey(nameof(GameId))]
         public int GameId { get; set; }
+        public Game Game { get; set; }
+
+       
     }
 }
