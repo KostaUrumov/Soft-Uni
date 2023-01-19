111fd74e4e983471e0a58c08a1141ae7c5cae585
KostaUrumov <koceurumov@gmail.com>
2023-01-19 21:03:57 +0000

    Book.cs

---
+++ EntityFramework/BookShop/BookShop/StartUp.cs
@@ -0,0 +1,93 @@
+﻿namespace BookShop
+{
+    using BookShop.Models;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+    using static System.Reflection.Metadata.BlobBuilder;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+            //string input = Console.ReadLine().ToLower();
+            //System.Console.WriteLine(GetBooksByAgeRestriction(db, input));
+            //Console.WriteLine(GetGoldenBooks(db));
+            Console.WriteLine(GetBooksByPrice(db));
+
+        }
+
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            StringBuilder result = new StringBuilder();
+            var booksTitle = context
+                .Books
+                .ToArray()
+                .Where(b => b.AgeRestriction.ToString().ToLower() == command)
+                .Select(b => b.Title)
+                .OrderBy(x => x);
+
+            foreach (var p in booksTitle)
+            {
+                result.AppendLine(p.ToString());
+            }
+            return result.ToString();
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            StringBuilder gold = new StringBuilder();
+            var pesho = context
+                .Books
+                .Where(x=> x.Copies < 5000)
+                .Select(x => new
+                {
+                   Titele = x.Title,
+                   Order = x.BookId,
+                   fff = x.EditionType
+                })
+                .OrderBy(x => x.Order)
+                .ToList();
+
+            foreach (var t in pesho)
+            {
+                if (t.fff.ToString() == "Gold")
+                {
+                    gold.AppendLine(t.Titele);
+                }
+               
+            }
+
+            return gold.ToString();
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+           
+            var tonkGera = context
+                .Books
+                .Where (c=> c.Price > 40)
+                .Select(c=> new
+                {
+                    Titles = c.Title,
+                    Pricee = c.Price,
+
+                })
+                .OrderByDescending(x=>x.Pricee)
+            .ToArray();
+
+
+            return string.Join(Environment.NewLine, tonkGera.Select(b => $"{b.Titles} - ${b.Pricee:f2}")); ;
+
+        }
+
+    }
+}
